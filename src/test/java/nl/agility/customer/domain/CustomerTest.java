bba0560ad2532e84cc8d86eda9bf39e88dba4d56
aduursma <andre@agility-it-services.nl>
2020-04-18 11:16:00 +0200

    Added testcases

--- src/test/java/nl/agility/customer/domain/CustomerTest.java
+++ src/test/java/nl/agility/customer/domain/CustomerTest.java
@@ -1,13 +1,47 @@
 package nl.agility.customer.domain;
 
+import com.google.common.testing.EqualsTester;
+import nl.agility.customer.CustomerMother;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SuppressWarnings("ALL")
 class CustomerTest {
 
-    @Test
-    void testMe() {
+    EqualsTester equalsTester;
 
+    @BeforeEach
+    void setUp() {
+        equalsTester = new EqualsTester();
     }
 
+    @Test
+    void customersWithDifferentIdsAreNotEqual() {
+        Customer customerOne = CustomerMother.complete().build();
+        Customer customerTwo = CustomerMother.complete().id(2L).build();
+
+        equalsTester
+            .addEqualityGroup(customerOne)
+            .addEqualityGroup(customerTwo)
+            .testEquals();
+    }
+
+    @Test
+    void verifyAllPropertiesArePartOfToString() throws Exception {
+        Customer customer = CustomerMother.complete().build();
+
+        for (Field field : FieldUtils.getAllFieldsList(customer.getClass())) {
+            var name = field.getName();
+            var value = FieldUtils.readField(field, customer, true);
+
+            assertThat(customer.toString(), containsString(String.format("%s=%s", name, value)));
+        }
+    }
 
 }
